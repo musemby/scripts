0d46c7ec9763ed45594a706bfbd2efe8f939db97
musembi <musembinzioki@gmail.com>
2016-11-24 19:49:31 +0300

    Added a git branch highlighting script

---
+++ highlight_git.sh
@@ -0,0 +1,13 @@
+# indicates which git branch one is currently working on
+# e.g musembi@server:~/scripts/ (fix-weird-bug)$ (currently on the fix-weird-bug branch)
+# paste this into the .bashrc file and source it
+function parse_git_branch () {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+RED="\[\033[0;31m\]"
+YELLOW="\[\033[0;33m\]"
+GREEN="\[\033[0;32m\]"
+NO_COLOR="\[\033[0m\]"
+
+PS1="$GREEN\u@\h$NO_COLOR:\w$YELLOW\$(parse_git_branch)$NO_COLOR\$ "
