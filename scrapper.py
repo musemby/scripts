f6004ef45aa1cd4819669c4a938a2c55ca5f3dc6
musembi <musembinzioki@gmail.com>
2016-12-01 23:43:07 +0300

    Added steps to the end

--- scrapper.py
+++ scrapper.py
@@ -4,10 +4,21 @@ from BeautifulSoup import BeautifulSoup
 url = 'https://www.lynda.com/Node-js-tutorials/Node-js-Essential-Training/417077-2.html'
 response = requests.get(url)
 html = response.content
-import pdb; pdb.set_trace()
-soup = BeautifulSoup(html)
-search_area = soup.find('div', attrs={'id': 'search-results-area'})
 
-for div in search_area.findAll('div', attrs={'class': 'col-xs-10 col-md-10 video-name-cont no-book'}):
-	title = div.find('a').text
-	print(title)
+soup = BeautifulSoup(html)
+search_area = soup.find('div', attrs={'id': 'toc-content'})
+import pdb; pdb.set_trace()
+course_toc = search_area.find('ul', attrs={'class': 'course-toc toc-container autoscroll autoscroll-disabled'})
+lips = course_toc.findAll('li', attrs={'role': 'presentation'})
+
+for lip in lips:
+	toci = lip.find('ul', attrs={'class': 'row toc-items'})
+	tocv = toci.findAll('li', attrs={'class': 'class="toc-video-item"'})
+	for each in tocv:
+		fin = each.find('div', attrs={'class': 'row video-row'}).find('div', {'class': 'col-xs-10 col-md-10 video-name-cont no-book'}).find('a')
+		print(fin.text)
+
+
+for row in table.findAll('tr'):
+    for cell in row.findAll('td'):
+        print cell.text
