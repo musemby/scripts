cbfae0a7587d3f2a5d9ad66b6d6edfc44d7a0faa
musembi <musembinzioki@gmail.com>
2016-12-03 18:50:09 +0300

    Added a more complete appproach towards ordinal suffix gen

--- ordinal_suffix.py
+++ ordinal_suffix.py
@@ -1,27 +1,24 @@
-import cmd
+import sys
 
-class GetOrdinal(cmd.Cmd):
-    '''
-        Return the ordinal suffix of a given number
-    '''
+'''
+    Return the ordinal suffix of a given number
+'''
 
-    def do_get_ordinal(self, number):
-        mod_ten = number % 10
-        mod_soo = number % 100
+def get_ordinal(number):
+    mod_ten = number % 10
+    mod_soo = number % 100
 
-        # 1,2,3 in teens are the exceptions
-        if mod_ten == 1 and mod_soo != 11:
-            return number + 'st'
-        if mod_ten == 2 and mode_soo != 12:
-            return number + 'nd'
-        if mod_ten == 3 and mod_soo != 13:
-            return number + 'rd'
+    # 1,2,3 in teens are the exceptions
+    if mod_ten == 1 and mod_soo != 11:
+        return str(number) + 'st'
+    if mod_ten == 2 and mode_soo != 12:
+        return str(number) + 'nd'
+    if mod_ten == 3 and mod_soo != 13:
+        return str(number) + 'rd'
 
-        return i + 'th'
-
-    def do_EOF(self, line):
-        return True
+    return str(number) + 'th'
 
 
 if __name__ == '__main__':
-    GetOrdinal().cmdloop()
+    num = int(sys.argv[1])
+    print(get_ordinal(num))
