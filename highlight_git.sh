a2afd746e653f214a35ebd1535902be4b1af8aad
musembi <musembinzioki@gmail.com>
2017-01-10 22:36:13 +0300

    Remove unused color RED

--- highlight_git.sh
+++ highlight_git.sh
@@ -1,11 +1,11 @@
 # indicates which git branch one is currently working on
-# e.g musembi@server:~/scripts/ (fix-weird-bug)$ (currently on the fix-weird-bug branch)
+# e.g musemby@server:~/scripts/ (fix-weird-bug)$ (currently on the fix-weird-bug branch)
 # paste this into the .bashrc file and source it
 function parse_git_branch () {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
-RED="\[\033[0;31m\]"
+# RED="\[\033[0;31m\]"
 GREEN="\[\033[0;32m\]"
 NO_COLOR="\[\033[0m\]"
 LIGHT_CYAN="\[\033[0;96m\]"
