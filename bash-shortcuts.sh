31ec201d3696fadbeb65fce81a59c81e5f63beb2
musembi <musembinzioki@gmail.com>
2017-02-02 10:30:23 +0300

    Updated the bash shortucts

--- bash-shortcuts.sh
+++ bash-shortcuts.sh
@@ -1,6 +1,9 @@
-# shortcuts
+# my aliases
+alias refresh='source ~/.bashrc'
 
 alias env='workon env'
+
+# emr
 alias backdir='cd ~/savannah/emr-backend'
 alias authdir='cd ~/savannah/emr-backend/bin'
 alias backend='env; backdir'
@@ -8,7 +11,16 @@ alias frontend='cd ~/savannah/emr-frontend'
 alias backendrun='backend; ./manage.py runserver'
 alias frontendrun='frontend; grunt connect'
 
-# git stuff
+# edi
+alias edidir='cd ~/savannah/slade360'
+alias edienv='workon slade'
+alias edi='edidir; edienv;'
+alias edirun='edi; ./manage.py runserver 8080'
+alias ediauth='workon auth; authserver_manage runserver 9000'
+alias providerdir='cd ~/savannah/sil-provider-portal'
+alias providerrun='providerdir; grunt connect:dev'
+
+# github
 alias gs='git status'
 alias gp='git pull'
 alias ga='git add'
@@ -17,6 +29,7 @@ alias gd='git diff'
 alias gc='git commit'
 alias gca='gaa; gc'
 alias gcm='git checkout master'
+
 alias auth='authdir; workon auth; python auth server'
 alias valueset='workon valueset; cd ~/savannah/slade360-valuesets-server/; ./manage.py runserver 8050'
 
@@ -25,3 +38,4 @@ alias gbncss='gbn; grunt build_css'
 alias gbn='grunt build_newer'
 
 alias p8='ping 8.8.8.8'
+
