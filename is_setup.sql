f62114961ac54c86a0c42dd2fc633a91e8c4de4f
musembi <musembinzioki@gmail.com>
2017-06-29 16:35:45 +0300

    Added drop role and db

--- is_setup.sql
+++ is_setup.sql
@@ -1,2 +1,5 @@
+DROP DATABASE IF EXISTS integration_services;
 CREATE DATABASE integration_services;
-GRANT ALL PRIVILEGES ON DATABASE integration_services TO app;
+DROP USER IF EXISTS integration_app;
+CREATE USER integration_app;
+GRANT ALL PRIVILEGES ON DATABASE integration_services TO integration_app;
