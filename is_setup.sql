1e4fb367e1ac8d24e09ddb0bfd331b1777046b20
musembi <musembinzioki@gmail.com>
2017-08-29 09:00:30 +0300

    Got rid of most of /scripts

--- is_setup.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS integration_services;
-CREATE DATABASE integration_services;
-DROP USER IF EXISTS integration_app;
-CREATE USER integration_app;
-GRANT ALL PRIVILEGES ON DATABASE integration_services TO integration_app;
